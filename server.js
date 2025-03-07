945ba69a929e8d5f0e133e5e7312f2a16ce00a81
Naveen Gowda MY <naveengowda23277@gmail.com>
2025-03-07 22:33:42 +0530

    cors updated

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require("cors"); // ✅ Import CORS
+const cors = require("cors");
 const downloadRoutes = require("./src/routes/downloadRoutes");
 
 const app = express();
@@ -7,20 +7,19 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // Allow requests from your frontend
-    credentials: true, // Allow credentials (cookies, authorization headers)
-    methods: "GET,POST,PUT,DELETE,OPTIONS", // Allow necessary HTTP methods
-    allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept, Authorization", // Allow necessary headers
+    origin: "https://musicvault-frontend.onrender.com/",
+    credentials: true,
+    methods: "GET,POST,PUT,DELETE,OPTIONS",
+    allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept, Authorization",
   })
 );
 
-// ✅ Allow JSON request body
 app.use(express.json());
 
-// Routes
+
 app.use("/api", downloadRoutes);
 
-// Start the server
+
 app.listen(PORT, () => {
   console.log(`Download Service running on http://localhost:${PORT}`);
 });
